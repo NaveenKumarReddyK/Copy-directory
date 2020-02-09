2a6b2bd12359473e93a5894242e89890b56b3523
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-02-09 16:48:20 +0530

    Update copyFIle.php

--- copyFIle.php
+++ copyFIle.php
@@ -1,35 +1,29 @@
 <?php
-// $oldf = fopen("clone.html","w");
-// $newf = fopen("copy.html","w");
-// $txt = "hi";
-// fwrite($newf,$txt);
-// copy("clone.html","copy.html");
-// fclose("copy.html");
-// fclose("clone.html");
-// $existing_file = fopen("clone.html","r");
-// fwrite($new_file,$existing_file);
+
 $src_dir = 'E:\XAMPP\htdocs\Copyfolder';
 $new_dir = 'E:\XAMPP\htdocs\NewCreatedFolder';
+// 0777 for file permissions to change the directory
 mkdir($new_dir,'0777');
 $new_file_dir = 'E:\XAMPP\htdocs\NewCreatedFolder\newfile.html';
 $old_file_dir = 'E:\XAMPP\htdocs\Copyfolder\clone.html';
+
+//create new file in the directory
 fopen($new_file_dir,"w");
 fopen($old_file_dir,"w");
+
+//copies file from old to new folder(not recomende)
 copy($new_file_dir,$old_file_dir);
+
 $curr_dir = getCwd();
 $arr = scandir($src_dir);
 print_r($arr);
+
+//recomended for ccopying all files
 foreach(scandir($src_dir) as $file){
     copy($src_dir.'/'.$file,$new_dir.'/'.$file);
 }
 echo $curr_dir;
 fclose($old_file_dir);
 fclose($new_file_dir);
-
-// if(!file_exists($new_dir)){
-  
-    // copy($new_dir,$src_dir);
-
-// }
 
 ?>
